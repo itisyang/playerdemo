53e28fa220123896c7574fb4676526da18a7d577
itisyang <itisyang@qq.com>
2018-03-15 20:46:11 +0800

    [mod]移除残留文件

--- playerdemo/videoctl.cpp
+++ playerdemo/videoctl.cpp
@@ -59,8 +59,6 @@
 
 #include <assert.h>
 
-const char program_name[] = "ffplay";
-const int program_birth_year = 2003;
 
 #define MAX_QUEUE_SIZE (15 * 1024 * 1024)
 #define MIN_FRAMES 25
@@ -3680,47 +3678,6 @@ static const OptionDef options[] = {
 };
 #endif
 
-static void show_usage(void)
-{
-    av_log(NULL, AV_LOG_INFO, "Simple media player\n");
-    av_log(NULL, AV_LOG_INFO, "usage: %s [options] input_file\n", program_name);
-    av_log(NULL, AV_LOG_INFO, "\n");
-}
-
-void show_help_default(const char *opt, const char *arg)
-{
-    //av_log_set_callback(log_callback_help);
-    show_usage();
-    //show_help_options(options, "Main options:", 0, OPT_EXPERT, 0);
-    //show_help_options(options, "Advanced options:", OPT_EXPERT, 0, 0);
-    printf("\n");
-    //show_help_children(avcodec_get_class(), AV_OPT_FLAG_DECODING_PARAM);
-    //show_help_children(avformat_get_class(), AV_OPT_FLAG_DECODING_PARAM);
-#if !CONFIG_AVFILTER
-    //show_help_children(sws_get_class(), AV_OPT_FLAG_ENCODING_PARAM);
-#else
-    show_help_children(avfilter_get_class(), AV_OPT_FLAG_FILTERING_PARAM);
-#endif
-    printf("\nWhile playing:\n"
-        "q, ESC              quit\n"
-        "f                   toggle full screen\n"
-        "p, SPC              pause\n"
-        "m                   toggle mute\n"
-        "9, 0                decrease and increase volume respectively\n"
-        "/, *                decrease and increase volume respectively\n"
-        "a                   cycle audio channel in the current program\n"
-        "v                   cycle video channel\n"
-        "t                   cycle subtitle channel in the current program\n"
-        "c                   cycle program\n"
-        "w                   cycle video filters or show modes\n"
-        "s                   activate frame-step mode\n"
-        "left/right          seek backward/forward 10 seconds\n"
-        "down/up             seek backward/forward 1 minute\n"
-        "page down/page up   seek backward/forward 10 minutes\n"
-        "right mouse click   seek to percentage in file corresponding to fraction of width\n"
-        "left double-click   toggle full screen\n"
-    );
-}
 
 static int lockmgr(void **mtx, enum AVLockOp op)
 {

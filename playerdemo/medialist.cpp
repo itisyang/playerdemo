3ab36de0222046544c27f92054cd941890afa468
itisyang <itisyang@gmail.com>
2018-07-26 22:10:56 +0800

    [add]增加播放列表类

---
+++ playerdemo/medialist.cpp
@@ -0,0 +1,10 @@
+#include "medialist.h"
+
+MediaList::MediaList(QObject *parent)
+    : QListWidget(parent)
+{
+}
+
+MediaList::~MediaList()
+{
+}
---
+++ playerdemo/medialist.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <QListWidget>
+
+class MediaList : public QListWidget
+{
+    Q_OBJECT
+
+public:
+    MediaList(QObject *parent);
+    ~MediaList();
+};
